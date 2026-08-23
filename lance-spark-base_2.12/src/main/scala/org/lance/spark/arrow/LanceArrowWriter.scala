f1b96049626ac9a6448c71c9267886e3c26a2ef2
shamirchen <shamirchen@tencent.com>
2026-08-24 00:46:36 +0800

    fix review

--- lance-spark-base_2.12/src/main/scala/org/lance/spark/arrow/LanceArrowWriter.scala
+++ lance-spark-base_2.12/src/main/scala/org/lance/spark/arrow/LanceArrowWriter.scala
@@ -25,8 +25,8 @@ import org.lance.spark.utils.{BlobReferenceResolver, BlobUtils, Float16Utils}
 import scala.collection.JavaConverters._
 
 /**
- * Custom ArrowWriter implementation that supports converting Spark DataFrame
- * Array<Float/Double> columns to Arrow FixedSizeList for vector embeddings.
+ * Custom ArrowWriter implementation that supports writing Spark ArrayType columns to Arrow
+ * FixedSizeList vectors.
  *
  * This class is copied and modified from Apache Spark's ArrowWriter
  * (https://github.com/apache/spark/blob/master/sql/catalyst/src/main/scala/org/apache/spark/sql/execution/arrow/ArrowWriter.scala)
@@ -77,7 +77,7 @@ object LanceArrowWriter {
       metadata: org.apache.spark.sql.types.Metadata = null,
       resolver: BlobReferenceResolver = null): LanceArrowFieldWriter = {
     (sparkType, vector) match {
-      case (ArrayType(elementType: NumericType, _), vector: FixedSizeListVector) =>
+      case (ArrayType(elementType, _), vector: FixedSizeListVector) =>
         val elementWriter = createFieldWriter(vector.getDataVector(), elementType, null, resolver)
         new FixedSizeListWriter(vector, elementWriter)
 
@@ -135,7 +135,9 @@ object LanceArrowWriter {
           null,
           resolver)
         new MapWriter(vector, structVector, keyWriter, valueWriter)
-      case (BinaryType, vector: StructVector) if BlobUtils.isBlobV2SparkMetadata(metadata) =>
+      case (BinaryType, vector: StructVector)
+          if BlobUtils.isBlobV2SparkMetadata(metadata) ||
+            BlobUtils.isBlobV2ArrowField(vector.getField) =>
         new BlobV2StructWriter(
           vector,
           createFieldWriter(vector.getChild("data"), BinaryType, null, resolver))
@@ -216,9 +218,13 @@ private[arrow] class FixedSizeListWriter(
     val elementWriter: LanceArrowFieldWriter) extends LanceArrowFieldWriter {
 
   override def setNull(): Unit = {
-    // Child vector must reserve listSize slots even for null rows,
-    // otherwise subsequent rows write to wrong offsets.
-    elementWriter.count += valueVector.getListSize()
+    // Child vectors must reserve listSize null slots even for null rows. Invoke the child writer for
+    // every slot so nested writers advance their descendants as well.
+    var i = 0
+    while (i < valueVector.getListSize()) {
+      elementWriter.writeNull()
+      i += 1
+    }
     valueVector.setNull(count)
   }
 
@@ -581,18 +587,18 @@ private[arrow] class BlobV2StructWriter(
 
   override def write(input: SpecializedGetters, ordinal: Int): Unit = {
     if (input.isNullAt(ordinal)) {
-      dataWriter.write(input, ordinal)
-      valueVector.setNull(count)
+      writeNull()
     } else {
       valueVector.setIndexDefined(count)
       dataWriter.write(input, ordinal)
+      count += 1
     }
-    count += 1
   }
 
-  // Rows are written through write(). setNull and setValue are no-ops so child counts stay
-  // owned by dataWriter.write().
-  override def setNull(): Unit = ()
+  override def setNull(): Unit = {
+    dataWriter.writeNull()
+    valueVector.setNull(count)
+  }
 
   override def setValue(input: SpecializedGetters, ordinal: Int): Unit = ()
 
@@ -634,8 +640,7 @@ private[arrow] class StructWriter(
     // index misalignment for all subsequent rows.
     var i = 0
     while (i < children.length) {
-      children(i).setNull()
-      children(i).count += 1
+      children(i).writeNull()
       i += 1
     }
     valueVector.setNull(count)
